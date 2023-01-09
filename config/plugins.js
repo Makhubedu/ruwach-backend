422963e54693c6b00a105399b12aa0276cb128f8
Makhubedu <lefaderrick@gmail.com>
2023-01-09 23:16:53 +0200

    Configure GraphQL

--- config/plugins.js
+++ config/plugins.js
@@ -14,17 +14,4 @@ module.exports = ({ env }) => ({
             },
         },
     },
-    // ...
-    graphql: {
-        config: {
-            endpoint: '/graphql',
-            shadowCRUD: true,
-            playgroundAlways: false,
-            depthLimit: 7,
-            amountLimit: 100,
-            apolloServer: {
-                tracing: false,
-            },
-        },
-    },
 });
